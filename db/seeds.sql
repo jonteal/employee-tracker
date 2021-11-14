0eefb8009f3f6eb13f63ab75a15e5b9f01d1e051
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-11-14 16:39:48 -0600

    Comments added to seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
 USE company_db;
 
+-- Numbers = the department ID number
 INSERT INTO department (name)
 VALUE ("Sales"), -- 1
         ("Finance"), -- 2
@@ -7,6 +8,8 @@ VALUE ("Sales"), -- 1
         ("Marketing"), --  4
         ("Web Development"); -- 5
 
+
+-- Numbers = the role ID number
 INSERT INTO role (title, salary, department_id)
 VALUEs ("Sales Manager", 100000, 1), -- 1
 ("Sales Representative", 40000, 1), -- 2
@@ -19,6 +22,8 @@ VALUEs ("Sales Manager", 100000, 1), -- 1
 ("Senior Developer", 120000, 5), -- 9
 ("Junior Developer", 60000, 5); -- 10
 
+
+-- Numbers = the employee ID number
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Rubeus", "Hagrid", 9, NULL), -- 1
         ("Severus", "Snape", 1, NULL), -- 2
@@ -27,6 +32,6 @@ VALUES ("Rubeus", "Hagrid", 9, NULL), -- 1
         ("Hermione", "Granger", 7, NULL), -- 5
         ("Ron", "Weasley", 8, 5), -- 6
         ("Draco", "Malfoy", 10, 1), -- 7
-        ("Albus", "Dumbledore", 6, 4),
-        ("Dudley", "Dursley", 2, 2),
-        ("Sirius", "Black", 4, 3);
+        ("Albus", "Dumbledore", 6, 4), -- 8
+        ("Dudley", "Dursley", 2, 2), -- 9
+        ("Sirius", "Black", 4, 3); -- 10
